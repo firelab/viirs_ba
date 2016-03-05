2ee1f72379870c9e9fc6f7fd9abbfd341c1ae388
bnordgren <bnordgren@fs.fed.us>
2016-03-05 19:49:51 +0000

    brute_force: tightened up range on RthSub

--- scripts/viirs_brute.py
+++ scripts/viirs_brute.py
@@ -12,7 +12,7 @@ def create_params(template, cls=vc.SequentialVIIRSConfig) :
     By default, the resultant configuration objects are of the "SequentialVIIRSConfig"
     type, but a different class may be specified via the cls parameter."""
     # for the moment,we're only varying two parameters.
-    RthSub_range = np.arange(0.02, 0.11, 0.02) # 0.02 - 0.10 by 0.02: 5 steps
+    RthSub_range = np.arange(0.03, 0.08, 0.01) # 0.03 - 0.07 by 0.01: 5 steps
     Rth_range    = np.arange(0.76, 0.85, 0.02) # 0.76-0.84 by 0.02: 5 steps
     
     template_vector = template.get_vector() 
