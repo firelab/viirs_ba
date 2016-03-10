ec4d61f9fd9c71cd9edba01cf513c2da47dc1c20
Bryce Nordgren <bnordgren@mine.usfs-i2.umt.edu>
2016-03-10 09:49:21 -0700

    changed viirs rasterize code to perform a bulk upsert for 750m fire events.

--- SqlFunctions/viirs_rasterize.sql
+++ SqlFunctions/viirs_rasterize.sql
@@ -41,35 +41,51 @@ $BODY$
 	EXECUTE 'ALTER TABLE ' || quote_ident(schema) || '.fire_events_raster ' ||
 	        'ADD COLUMN rast_750 raster'  ; 
 
-	EXECUTE 'UPDATE ' || quote_ident(schema) || '.fire_events_raster arast ' ||
-	  'SET rast_750 = dummy.rast_750 ' || 
-	  'FROM (SELECT b.rid, ' ||
-	     'ST_MapAlgebra(' ||
-		'ST_Union(ST_AsRaster(geom_nlcd, empty_rast_750.rast, ' || quote_literal('8BUI') ||')), '
-		'empty_rast_750.rast, ' ||
-		quote_literal('[rast1]') || ', ' || 
-		quote_literal('8BUI') || ', ' || 
-		quote_literal('SECOND') || ') as rast_750 ' ||
-	    'FROM ' || quote_ident(schema) || '.fire_events a, ' || 
-	               quote_ident(gt_schema) || '.' || quote_ident(gt_table) || ' b, ' ||
-	             '(SELECT rid, St_SetSRID(ST_AddBand(ST_MakeEmptyRaster(ST_Width(rast)/2,' ||
-		                              'ST_Height(rast)/2,' || 
-		                              'ST_UpperLeftX(rast),' ||
-		                              'ST_UpperLeftY(rast), 750), ' ||
-		          quote_literal('8BUI') || '::text), ST_SRID(rast)) as rast ' ||
-		       'FROM ' || 
-		       quote_ident(gt_schema) || '.' || quote_ident(gt_table) || 
-		       ') empty_rast_750 ' || 
-	    'WHERE ST_Intersects(geom_nlcd, b.rast) AND ' ||
-	        'ST_DWithin(a.geom_nlcd, b.geom, $1) AND ' ||
-	        'b.rid = empty_rast_750.rid AND ' ||
-	        'pixel_size = 750 ' ||  
-	    'GROUP BY b.rid, empty_rast_750.rast) dummy ' ||
-	  'WHERE arast.rid=dummy.rid'  USING distance;  
 
-	EXECUTE 'UPDATE ' || quote_ident(schema) || '.fire_events_raster ' ||
-	    'SET rast_750 = ST_SetBandNoDataValue(rast_750, 3.)' ;
-	      
+    CREATE TEMPORARY TABLE newrasters (rid integer, rast_750 raster) ; 
+
+    EXECUTE  'INSERT INTO newrasters ' || 
+      'SELECT b.rid, ST_MapAlgebra(' ||
+		    'ST_Union(ST_AsRaster(geom_nlcd, empty_rast_750.rast, '|| 
+                         quote_literal('8BUI') || ')), empty_rast_750.rast, ' ||
+		    quote_literal('[rast1]') || ', ' || 
+		    quote_literal('8BUI') || ', ' || 
+		    quote_literal('SECOND') || ') as rast_750 ' ||
+      'FROM ' || quote_ident(schema) || '.fire_events a, ' || 
+           quote_ident(gt_schema) || '.' || quote_ident(gt_table) || ' b, ' ||
+           '(SELECT rid, ' || 
+	           'St_SetSRID(ST_AddBand(ST_MakeEmptyRaster(ST_Width(rast)/2, ' ||
+		                                  'ST_Height(rast)/2, ' ||
+		                                  'ST_UpperLeftX(rast), ' ||
+		                                  'ST_UpperLeftY(rast), 750), ' ||
+		             quote_literal('8BUI')||'::text), ST_SRID(rast)) as rast ' ||
+            'FROM ' || quote_ident(gt_schema)||'.'||quote_ident(gt_table)||') empty_rast_750 ' ||
+      'WHERE ST_Intersects(geom_nlcd, b.rast) AND ' ||
+	            'ST_DWithin(a.geom_nlcd, b.geom, $1) AND ' ||
+	            'b.rid = empty_rast_750.rid AND ' ||
+	            'pixel_size = 750  ' ||
+      'GROUP BY b.rid, empty_rast_750.rast ' USING distance;
+    
+    EXECUTE 'LOCK TABLE ' || quote_ident(schema) || '.fire_events_raster ' ||
+          'IN EXCLUSIVE MODE' ; 
+    
+    EXECUTE 'UPDATE  ' || quote_ident(schema) || '.fire_events_raster me '
+      'SET rast_750 = newrasters.rast_750 ' ||
+      'FROM newrasters ' ||
+      'WHERE newrasters.rid = me.rid' ; 
+
+    EXECUTE 'INSERT INTO ' || quote_ident(schema) || '.fire_events_raster ' ||
+       '(rid, rast_750) ' || 
+       'SELECT newrasters.rid, newrasters.rast_750 ' || 
+       'FROM newrasters ' || 
+       'LEFT OUTER JOIN ' || quote_ident(schema) || '.fire_events_raster me ' ||
+          'ON (newrasters.rid = me.rid) ' || 
+       'WHERE me.rid IS NULL' ;
+
+    EXECUTE 'UPDATE ' || quote_ident(schema) || '.fire_events_raster ' ||
+	    'SET rast_750 = ST_SetBandNoDataValue(rast_750, 3.) ' ||
+            'WHERE rast_750 IS NOT NULL' ;
+
     END
 $BODY$ 
   LANGUAGE plpgsql VOLATILE
