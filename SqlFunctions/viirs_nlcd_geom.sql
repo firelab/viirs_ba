aaf787f778ffb0ad5e27f8e58a98599b39655ec7
Bryce Nordgren <bnordgren@mine.usfs-i2.umt.edu>
2016-03-16 13:16:02 -0600

    generalized viirs_nlcd_fire_events to reproject other tables.

--- SqlFunctions/viirs_nlcd_geom.sql
+++ SqlFunctions/viirs_nlcd_geom.sql
@@ -1,20 +1,23 @@
-CREATE OR REPLACE FUNCTION viirs_nlcd_fire_events(schema text) 
+CREATE OR REPLACE FUNCTION viirs_nlcd_geom(schema text, tbl text) 
    RETURNS void AS
 $BODY$
     BEGIN
 
-    EXECUTE 'ALTER TABLE ' || quote_ident(schema) || '.fire_events ' ||
-              'DROP COLUMN IF EXISTS geom_nlcd' ;
-    EXECUTE 'ALTER TABLE ' || quote_ident(schema) || '.fire_events ' || 
-              'ADD COLUMN geom_nlcd geometry(Multipoint, 96630)'; 
+    EXECUTE 'ALTER TABLE ' || quote_ident(schema) || '.' || 
+              quote_ident(tbl) || 
+              ' DROP COLUMN IF EXISTS geom_nlcd' ;
+    EXECUTE 'ALTER TABLE ' || quote_ident(schema) || '.' || 
+              quote_ident(tbl) || 
+              ' ADD COLUMN geom_nlcd geometry'; 
               
-    EXECUTE 'UPDATE ' || quote_ident(schema) || '.fire_events ' ||
-             'SET geom_nlcd = ST_Transform(geom, 96630)';
+    EXECUTE 'UPDATE ' || quote_ident(schema) || '.' ||
+             quote_ident(tbl) || 
+             ' SET geom_nlcd = ST_Transform(geom, 96630)';
     
     END
 $BODY$ 
   LANGUAGE plpgsql VOLATILE
   COST 100 ; 
-ALTER FUNCTION viirs_nlcd_fire_events(schema text)
+ALTER FUNCTION viirs_nlcd_geom(schema text, tbl text)
   OWNER to postgres ;
 
