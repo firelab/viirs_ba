6bcf9c5a0e1322f14cfd701316cc915dad5b8402
Bryce Nordgren <bnordgren@mine.usfs-i2.umt.edu>
2016-03-17 11:24:18 -0600

    new feature: polygon zone figure of merit

---
+++ SqlFunctions/viirs_zone.sql
@@ -0,0 +1,68 @@
+CREATE OR REPLACE FUNCTION viirs_zonetbl_init(schema text, tbl text, srid int)
+   RETURNS void AS
+$BODY$
+    BEGIN
+
+    EXECUTE 'DROP TABLE IF EXISTS ' || 
+           quote_ident(schema) ||'.'||quote_ident(tbl) ; 
+
+    EXECUTE 'CREATE TABLE ' || 
+           quote_ident(schema) || '.' || quote_ident(tbl) || 
+           ' (geom geometry(Multipoint, ' ||
+            srid::text ||'), zone int, run_id text, ' ||
+           'cells375 int, area_km real)' ; 
+           
+    END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100 ;
+ALTER FUNCTION viirs_zonetbl_init(schema text, tbl text, srid int)
+  OWNER to postgres ;
+
+-- viirs_zonetbl_run() aggregates rasterized fire_events from a 
+--   single run spatially, using polygons defined in a zone 
+--   definition table. It requires that the table in which 
+--   results are accumulated already exist (i.e., call viirs_zonetbl_init).
+--   While the terminology used here is "zone", any table having a 
+--   Polygon geometry column and a unique id column will do. This 
+--   code considers each row in such a table to be a zone.
+-- zone_schema.zonedef_tbl defines the reference polygon set
+-- zone_schema.zone_tbl    is where the results from all the runs are 
+--                         accumulated
+-- run_schema.fire_events_raster must exist and must have a geom column
+--
+-- The zone_col parameter is the name of the unique identifier column.
+CREATE OR REPLACE FUNCTION viirs_zonetbl_run(zone_schema text, zone_tbl text, 
+        zonedef_tbl text, run_schema text, zone_col text)
+   RETURNS void AS
+$BODY$
+    BEGIN
+
+    -- Carve out only the points which intersect the reference polygons
+    EXECUTE 'CREATE TEMPORARY TABLE intersections ON COMMIT DROP AS ' ||
+            'SELECT ST_Multi(ST_Intersection(a.geom,b.geom_nlcd)) as geom, '||
+                   'a.zone ' ||
+            'FROM ' || quote_ident(zone_schema)||'.'||quote_ident(zonedef_tbl)||' a,'||
+                    quote_ident(run_schema)||'.fire_events_raster b ' || 
+            'WHERE ST_Intersects(a.geom, b.geom_nlcd)' ; 
+
+    -- Group the intersecting points by zone and insert
+    -- into the master table.
+    EXECUTE 'INSERT INTO ' || 
+            quote_ident(zone_schema) ||'.'|| quote_ident(zone_tbl) || 
+            ' (geom, zone, run_id, cells375, area_km) '  ||
+            'SELECT ST_Multi(ST_Collect(f.geom)), ' || 
+                   'f.zone, ' || quote_literal(run_schema) || ', ' ||
+                   'SUM(ST_NPoints(f.geom)), ' ||
+                   'SUM(ST_NPoints(f.geom))*0.140625 ' ||
+            'FROM (SELECT ' || quote_ident(zone_col) ||
+                    ' as zone, (ST_Dump(geom)).geom as geom ' ||
+                   'FROM intersections) as f ' || 
+            'GROUP BY ' || quote_ident(zone_col) ;
+
+    END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100 ;
+ALTER FUNCTION viirs_zonetbl_run(text, text, text, text, text)
+  OWNER to postgres ;
--- scripts/viirs_fom.py
+++ scripts/viirs_fom.py
@@ -3,11 +3,22 @@
 In general, this module contains components to implement the following flow: 
     + add a column to the fire_events table to store the points projected into
       the NLCD projection.
-    + create a fire_events_raster serving as a fire mask
-    + perform intersections and unions between the satellite fire mask and 
-      the ground truth fire mask.
+    + create two fire_events_rasters serving as a fire mask for 750m and 
+      375m fire events points.
+    + combine the two fire events rasters on a 375m grid.
+    + perform intersections and unions between the combined satellite 
+      fire mask and the ground truth fire mask.
     + sum all the active fire pixels in the intersection and the union.
     + ratio the sum of the intersection pixels to the sum  of union pixels.
+
+A second figure of merit flow is as follows: 
+    + add a column to the fire_events table to store the points projected into
+      the NLCD projection.
+    + create two fire_events_rasters serving as a fire mask for 750m and 
+      375m fire events points.
+    + combine the two fire events rasters on a 375m grid.
+    + sum all "true" points in the combined fire events mask on a per zone
+      basis, storing the results in a table
 """
 
 import os.path
@@ -20,10 +31,10 @@ import VIIRS_threshold_reflCor_Bulk as vt
 import viirs_config as vc
 
 
-def extract_fire_mask(config, gt_schema, gt_table) : 
+def extract_fire_mask(config, gt_schema, gt_table,rast_col='rast',geom_col='geom') : 
     """Creates a geometry column and populates with pixel centers where pixel value=1"""
-    query = "SELECT viirs_get_mask_pts('{0}','{1}', 'rast', 'geom', {2})".format(
-          gt_schema, gt_table, vt.srids['NLCD'])
+    query="SELECT viirs_get_mask_pts('{0}', '{1}', '{2}', '{3}', {4})".format(
+         gt_schema, gt_table, rast_col, geom_col, vt.srids['NLCD'])
     vt.execute_query(config, query) 
 
 def project_fire_events_nlcd(config) :
@@ -33,7 +44,6 @@ def project_fire_events_nlcd(config) :
     query = "SELECT viirs_nlcd_geom('{0}', 'fire_events', {1})".format(config.DBschema, vt.srids["NLCD"])
     vt.execute_query(config, query)
 
-                            
 def create_fire_events_raster(config, gt_schema, gt_table) : 
     """dumps the ground truth fire mask to disk, overwrites by rasterizing fire_events, 
     reloads the table to postgis
@@ -94,6 +104,30 @@ def do_ioveru_fom(gt_schema, gt_table, config) :
     create_fire_events_raster(config, gt_schema, gt_table)
     mask_sum(config, gt_schema, gt_table)
     return calc_ioveru_fom(config)
+
+def zonetbl_init(zone_schema, zone_tbl, config) : 
+    """drops and re-creates the table in which results are accumulated"""
+    query = "SELECT viirs_zonetbl_init('{0}', '{1}', {2})".format(
+           zone_schema, zone_tbl, vt.srids['NLCD'])
+    vt.execute_query(config, query)
+
+def zonetbl_run(zone_schema, zonedef_tbl, zone_tbl, zone_col, config) : 
+    run_schema = config.DBschema
+    
+    query="SELECT viirs_zonetbl_run('{0}','{1}','{2}','{3}','{4}')".format(
+        zone_schema, zone_tbl, zonedef_tbl, run_schema, zone_col)
+    vt.execute_query(config, query)
+
+def do_one_zonetbl_run(gt_schema, zonedef_tbls, zone_tbls, zone_cols, config):
+    """accumulates fire points from a single run into one or more zone tables.
+    The zone definition table, results accumulation table, and column names
+    are specified as parallel lists in zonedef_tbls, zone_tbls, zone_cols.
+    """
+    extract_fire_mask(config, config.DBschema, 'fire_events_raster',
+                       geom_col='geom_nlcd')
+    # try treating deftbls, tbls, and cols as parallel lists
+    for deftbl,tbl,col in zip(zonedef_tbls,zone_tbls,zone_cols) : 
+        zonetbl_run(gt_schema, deftbl, tbl, col, config)
     
 def calc_all_ioveru_fom(run_datafile, gt_schema, gt_table, workers=1) : 
     """calculates the i over u figure of merit for a batch of previously
@@ -110,6 +144,10 @@ def calc_all_ioveru_fom(run_datafile, gt_schema, gt_table, workers=1) :
     runlist = pd.read_csv(run_datafile, index_col=0) 
     fomdata = np.zeros_like(runlist['run_id'],dtype=np.float)
     config_list = vc.VIIRSConfig.load_batch(base_dir)
+
+    # prep ground truth table, giving each row a multipoint
+    # geometry of the centroids of "true" pixels in the mask
+    extract_fire_mask(config, gt_schema, gt_table)
 
     workerfunc = ft.partial(do_ioveru_fom, gt_schema, gt_table)
 
@@ -128,3 +166,23 @@ def calc_all_ioveru_fom(run_datafile, gt_schema, gt_table, workers=1) :
     newname = 'new_{0}'.format(os.path.basename(run_datafile))
     runlist.to_csv(os.path.join(base_dir, newname))
 
+def do_all_zonetbl_runs(base_dir, gt_schema, workers=1) : 
+    """accumulates fire event raster points by polygon-defined zones.
+    This function relies on the rasterized fire events tables created
+    by the do_ioveru_fom() method. Make sure that these tables exist.
+    """
+    config_list = vc.VIIRSConfig.load_batch(base_dir)
+
+    # prepare the table to accumulate results
+    zonetbl_init(gt_schema, 'eval_zone_counts', config_list[0])
+
+    workerfunc = ft.partial(do_one_zonetbl_run, gt_schema,
+                      ('dissolve_eval_zones',),
+                      ('eval_zone_counts',),
+                      ('zone',))
+
+    if workers == 1 : 
+        map(workerfunc, config_list)
+    else:  
+        mypool = mp.Pool(processes=workers)
+        mypool.map(workerfunc,config_list)
